bc602d8619f7d6b6e366d79ba3334ef7ea378556
nicerobot <git@nicerobot.org>
2010-03-17 20:05:46 -0400

    Adding scala to same project. Changing pleach.sh to better handle $1 input.

--- pleac.sh
+++ pleac.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-find src/main/java -name "_${1:-*}.java" -print0 \
+find src/main/java src/main/scala -name "${1:-_*.*}" -type f -print0 \
   | xargs -I{} -n1 sed -n '/<PLEAC>/,/<[/]PLEAC>/p' \{\} \
   | grep -v 'PLEAC>' \
   | perl -pi -e 's/^\s{6,6}//' \
